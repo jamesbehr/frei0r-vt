1fa71ad51ba5ba948eb582fab51e6d8189bd34b3
James Behr <jamesbehr@gmail.com>
2023-11-08 21:36:29 +0200

    Draw background

--- src/lib.rs
+++ src/lib.rs
@@ -119,7 +119,7 @@ pub extern "C" fn f0r_set_param_value(inst: *mut Vt, param: *mut libc::c_void, i
                         vt.feed_str(&data);
 
                         let data = vt
-                            .view()
+                            .lines()
                             .iter()
                             .map(|line| line.cells().collect())
                             .collect();
@@ -230,6 +230,21 @@ struct Frame {
     data: Vec<Vec<(char, avt::Pen)>>,
 }
 
+struct Dimensions {
+    row_height: f32,
+    col_width: usize,
+}
+
+impl Dimensions {
+    fn x(&self, col: usize) -> usize {
+        col * self.col_width
+    }
+
+    fn y(&self, row: usize) -> usize {
+        (row as f32 * self.row_height).round() as usize
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn f0r_update(
     inst: *mut Vt,
@@ -240,13 +255,19 @@ pub extern "C" fn f0r_update(
     let inst = unsafe { &*inst };
 
     // TODO: Glyph caching
+    // TODO: Better font (extra chars)
     let font = include_bytes!("../font/ibm-plex-mono-latin-400-normal.ttf") as &[u8];
     let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
     let font_size = 16.0; // TODO: Param
 
     let size = (inst.width * inst.height) as usize;
-    let advanced_width = font.metrics('0', font_size).width;
-    let row_height = font_size * 1.0;
+    let col_width = font.metrics('0', font_size).width;
+    let row_height = font_size * 1.3;
+
+    let dim = Dimensions {
+        row_height,
+        col_width,
+    };
 
     let theme = Theme {
         background: RGB8::new(0x12, 0x13, 0x14),
@@ -280,6 +301,7 @@ pub extern "C" fn f0r_update(
         data: vec![],
     };
 
+    // TODO: Frame timing for long videos isn't working (should stick on last frame)
     let frame = inst
         .frames
         .iter()
@@ -290,6 +312,24 @@ pub extern "C" fn f0r_update(
         for (col, (char, pen)) in line.iter().enumerate() {
             let (metrics, bitmap) = font.rasterize(*char, font_size);
 
+            // Draw the background over the whole cell
+            if let Some(c) = pen.background() {
+                for y in dim.y(row)..dim.y(row + 1) {
+                    for x in dim.x(col)..dim.x(col + 1) {
+                        let stride = inst.width as usize;
+
+                        if (x as u32) < inst.width && (y as u32) < inst.height {
+                            v[y as usize * stride + x as usize] =
+                                rgb_to_u32(color_to_rgb(c, &theme).into());
+                        }
+                    }
+                }
+            }
+
+            if char == &' ' {
+                continue;
+            }
+
             for gy in 0..metrics.height {
                 for gx in 0..metrics.width {
                     let pixel = bitmap[gy * metrics.width + gx];
@@ -298,7 +338,7 @@ pub extern "C" fn f0r_update(
                         - metrics.height as i32
                         - metrics.ymin;
 
-                    let x = (col * advanced_width) as i32 + gx as i32 + metrics.xmin;
+                    let x = (col * col_width) as i32 + gx as i32 + metrics.xmin;
                     let stride = inst.width as usize;
 
                     let bg = pen
@@ -313,7 +353,9 @@ pub extern "C" fn f0r_update(
                         .unwrap_or(theme.foreground)
                         .alpha(255);
 
-                    v[y as usize * stride + x as usize] = rgb_to_u32(blend(fg, bg, pixel));
+                    if (x as u32) < inst.width && (y as u32) < inst.height {
+                        v[y as usize * stride + x as usize] = rgb_to_u32(blend(fg, bg, pixel));
+                    }
                 }
             }
         }
